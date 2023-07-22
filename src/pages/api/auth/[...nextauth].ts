aa637057eadd2db8e229dbb27dba1cfbe102233d
Nidhish Pareek <pareek.np1@gmail.com>
2023-07-22 19:51:33 +0530

    prisma working fine now

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 const prisma = new PrismaClient();
 
@@ -10,8 +9,8 @@ export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT as string,
-      clientSecret: process.env.GOOGLE_SECRET_KEY as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 });
