c420e9655a2e6c8a3ce667508e86851df259971c
Nidhish Pareek <pareek.np1@gmail.com>
2023-07-22 21:58:10 +0530

    visualization of user data

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+};
+module.exports = nextConfig;
