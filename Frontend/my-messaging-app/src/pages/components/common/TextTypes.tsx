71e9663bfacd2e7f03f52f2d6bca96329a75b439
Nidhish Pareek <102843788+nidhishpareek@users.noreply.github.com>
2023-07-25 23:48:00 +0530

    Revert "Change root directory of project"

--- Frontend/my-messaging-app/src/pages/components/common/TextTypes.tsx
+++
@@ -1,5 +0,0 @@
-import { FONT_SIZE } from "@/theme/constants";
-import { Text } from "@chakra-ui/react";
-
-export const Title = ({ children = "" }) =>
-  children ? <Text fontSize={FONT_SIZE.popupHeading}>{children}</Text> : null;
